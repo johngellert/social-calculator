e93b5015d36877c6aa095cf55d5a7a48e28132f4
John Gellert <gellertjm@gmail.com>
2019-09-06 14:48:10 -0500

    Add database.sql and update pool.js with database name.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,29 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import {
+  HashRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from "react-router-dom";
+import { connect } from "react-redux";
+
+import ProtectedRoute from "../ProtectedRoute/ProtectedRoute";
+
+import AboutPage from "../AboutPage/AboutPage";
+import UserPage from "../UserPage/UserPage";
+import InfoPage from "../InfoPage/InfoPage";
+import NavDrawer from "../NavDrawer/NavDrawer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>
-          Social Calculator
-        </h1>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <h1>Social Calculator</h1>
+        </header>
+      </div>
+    </Router>
   );
 }
 

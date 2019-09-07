7cfd9f7e0d0b13ff5030aa4b0054293b962607a6
John Gellert <gellertjm@gmail.com>
2019-09-07 10:04:05 -0500

    Update database.sql file with operations table.

--- database.sql
+++ database.sql
@@ -3,3 +3,10 @@ CREATE TABLE "user" (
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+
+CREATE TABLE "operations" (
+    "id" SERIAL PRIMARY KEY,
+    "user_id" INT REFERENCES "user" NOT NULL,
+    "username" VARCHAR (80) NOT NULL,
+    "operation" VARCHAR (1000) NOT NULL
+);
